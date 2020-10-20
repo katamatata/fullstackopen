bc6d32d87b5c25c03b532f415f552db187bfd575
katamatata <51786805+katamatata@users.noreply.github.com>
2020-10-20 13:42:16 +0200

    Add solutions for exercises 1.1-1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -10,19 +10,31 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  const Header = (props) => (
+    <h1>{props.course}</h1>
+  );
+
+  const Part = (props) => (
+  <p>{props.part} {props.exercises}</p>
+  );
+
+  const Content = (props) => (
+   <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+   </div>
+  );
+
+  const Total = (props) => (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  );
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
