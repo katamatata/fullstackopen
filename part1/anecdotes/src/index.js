eabc9bf69870b5244abda78ac0709af167f0702d
katamatata <51786805+katamatata@users.noreply.github.com>
2020-11-09 16:29:44 +0100

    Add solution for exercise 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -6,20 +6,31 @@ const Button = ({handleClick, text}) => (
 );
 
 const App = (props) => {
+  const points = new Array(anecdotes.length).fill(0);
+
+  const [votes, setVotes] = useState(points);
   const [selected, setSelected] = useState(0);
 
-const onButtonClicked = () => {
-  const random = Math.floor(Math.random() * anecdotes.length);
-  setSelected(random);
-}
+  const onNextClicked = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
+
+  const onVoteClicked = (selected) => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
 
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={onButtonClicked} text="Next anecdote" />
+      <p>This anecdote has {votes[selected]} votes.</p>
+      <Button handleClick={onNextClicked} text="Next anecdote" />
+      <Button handleClick={() => onVoteClicked(selected)} text="Vote" />
     </div>
-  )
-}
+  );
+};
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -28,9 +39,9 @@ const anecdotes = [
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+];
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
   document.getElementById('root')
-)
+);
