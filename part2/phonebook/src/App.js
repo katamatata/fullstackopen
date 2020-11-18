a2acd1f0c60a5168b439e77549d69c16f8bebfc4
katamatata <51786805+katamatata@users.noreply.github.com>
2020-11-18 23:20:15 +0100

    Add solution for exercises 2.9-2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import Filter from './components/Filter';
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -39,9 +46,18 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchInputChange = (event) => {
+    // console.log(event.target.value);
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter
+        searchInputValue={search}
+        handleSearchChange={handleSearchInputChange}
+      />
       <h3>Add a new contact</h3>
       <PersonForm
         handleSubmit={addPerson}
@@ -52,7 +68,7 @@ const App = () => {
       />
 
       <h3>Contacts</h3>
-      <Persons persons={persons} />
+      <Persons persons={persons} search={search} />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const Persons = ({ persons }) => (
-  <div>
-    {persons.map((person) => (
-      <p key={person.name}>
-        {person.name} {person.number}
-      </p>
-    ))}
-  </div>
-);
+const Persons = ({ persons, search }) => {
+  const contactsToRender =
+    search === ''
+      ? persons
+      : persons.filter((person) => person.name.includes(search));
+
+  return (
+    <div>
+      {contactsToRender.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export default Persons;
