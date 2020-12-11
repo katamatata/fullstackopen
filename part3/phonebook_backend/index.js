7438a65c60caeffe78037e8604e4a2b0b3a6de55
katamatata <51786805+katamatata@users.noreply.github.com>
2020-12-11 22:49:09 +0100

    Update contacts.js, create production build

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 app.use(express.json());
+app.use(express.static('build'));
 app.use(cors());
 
 morgan.token('data', function (req, res) {
