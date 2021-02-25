6bbb1fd029e0647e98b7cbd63f7c7bd8a9e96711
katamatata <51786805+katamatata@users.noreply.github.com>
2021-02-25 11:51:26 +0100

    Move Redux-store into its own file store.js, ex. 6.9

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
+import store from './store';
 import App from './App';
-import reducer from './reducers/anecdoteReducer';
-
-const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -13,7 +13,7 @@ const anecdotesAtStart = [
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_VOTE: {
       const id = action.data.id;
@@ -33,4 +33,4 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-export default reducer;
+export default anecdoteReducer;
